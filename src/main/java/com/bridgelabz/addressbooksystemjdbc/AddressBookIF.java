e9a3486133a9dbd1a046146b11a27084246332bd
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-26 01:35:55 -0800

    Added:files from previous address book sysytem repository

---
+++ src/main/java/com/bridgelabz/addressbooksystemjdbc/AddressBookIF.java
@@ -0,0 +1,45 @@
+package com.bridgelabz.addressbooksystemjdbc;
+
+import java.io.IOException; 
+import java.util.List;
+import java.util.Scanner;
+
+import com.bridgelabz.addressbooksystemjdbc.AddressBook.IOService;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+
+
+public interface AddressBookIF {
+
+	public void operation();
+	
+	public void createContactPerson(Scanner scannerObject);
+
+	public void addContact(String firstName, ContactPerson person);
+
+	public void displayContents();
+
+	public void editPerson();
+
+	public void deletePerson();
+
+	public void addPersonToCity(ContactPerson contact);
+
+	public void addPersonToState(ContactPerson contact);
+
+	public void printSortedList(List<ContactPerson> sortedContactList);
+
+	public void sortAddressBook(int sortingChoice);
+	
+	public void writeToAddressBookFile(IOService ioService);
+
+	public List<String> readDataFromFile(IOService fileIo);
+	
+	public void writeDataToCSV() throws IOException, CsvRequiredFieldEmptyException, CsvDataTypeMismatchException;
+	
+	public <CsvValidationException extends Throwable> void readDataFromCSV() throws IOException, CsvValidationException;
+
+	public void writeDataToJson() throws IOException;
+	
+	public void readDataFromJson() throws IOException;
+}
