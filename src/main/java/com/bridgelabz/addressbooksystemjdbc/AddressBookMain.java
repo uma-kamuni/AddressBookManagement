c29c95364e32d22f90e682f051ec31bd8961989c
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-26 01:36:27 -0800

    Added:files from previous address book sysytem repository

--- src/main/java/com/bridgelabz/addressbooksystemjdbc/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbooksystemjdbc/AddressBookMain.java
@@ -1,9 +1,17 @@
 package com.bridgelabz.addressbooksystemjdbc;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 
+	public static Scanner scannerObject = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book System JDBC");
+
+		System.out.println("Welcome To Address Book Program");
+		AddressBookDirectory addressBookDirectory = new AddressBookDirectory();
+		addressBookDirectory.operationDirectory();
+		
 	}
 
 }
