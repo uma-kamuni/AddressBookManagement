cad8645ace963627bcf7865cf94505e942c4d5cf
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-26 01:40:26 -0800

    Refactored: Establishing connection between java and database

--- src/main/java/com/bridgelabz/addressbooksystemjdbc/AddressBookDBService.java
+++ src/main/java/com/bridgelabz/addressbooksystemjdbc/AddressBookDBService.java
@@ -1,14 +1,11 @@
 package com.bridgelabz.addressbooksystemjdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.Connection;import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class AddressBookDBService {
 
